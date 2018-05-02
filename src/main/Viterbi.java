21b7cd902d0f7062a89dd7f64e57b1b89b036b5e
AlexQianYi <q563875735@gmail.com>
2018-05-02 17:11:53 -0400

    11

---
+++ src/main/Viterbi.java
@@ -0,0 +1,22 @@
+package main;
+
+public class Viterbi {
+	
+	private int length = 0;
+	private int[] Obverse = null;
+	
+	public Viterbi(int l, int[] input) {
+		this.length = l;
+		this.Obverse = new int[length];
+		this.Obverse = input;	
+	}
+	
+	public double runViterbi() {
+		double result = 0.0;
+		
+		
+		
+		return result;
+	}
+
+}
